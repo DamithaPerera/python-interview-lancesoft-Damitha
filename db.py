4ab3dcaa2afb6da663e6dde5cba5939cdbb4ac5f
DamithaPerera <damitha@supplied.eu>
2026-02-15 19:20:34 +0530

    changes

--- db.py
+++ db.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
+import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "sqlite:///./fx.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./fx.db")
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
